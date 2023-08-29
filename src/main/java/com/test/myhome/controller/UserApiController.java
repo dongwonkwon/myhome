5a7013a6a6d82eada7b4290879c7d67c8e76c13e
dongwonkwon <90139277+dongwonkwon@users.noreply.github.com>
2023-08-30 02:32:22 +0900

    feat: 권한에 맞는 화면 구성 및 API 호출 - 사용자 권한에 따라 다른 UI 구성 - 사용자 권한에 따라 API 제한

--- src/main/java/com/test/myhome/controller/UserApiController.java
+++ src/main/java/com/test/myhome/controller/UserApiController.java
@@ -3,6 +3,7 @@ package com.test.myhome.controller;
 import com.test.myhome.model.Board;
 import com.test.myhome.model.User;
 import com.test.myhome.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.thymeleaf.util.StringUtils;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@Slf4j
 class UserApiController {
 
     @Autowired
@@ -21,7 +23,11 @@ class UserApiController {
     // tag::get-aggregate-root[]
     @GetMapping("/users")
     List<User> all() {
-        return userRepository.findAll();
+        List<User> users = userRepository.findAll();
+        log.debug("getBoards().size() 호출전");
+        log.debug("getBoards().size() : {}", users.get(0).getBoards().size());
+        log.debug("getBoards().size() 호출후");
+        return users;
     }
     // end::get-aggregate-root[]
 
--- src/main/java/com/test/myhome/model/User.java
+++ src/main/java/com/test/myhome/model/User.java
@@ -21,6 +21,7 @@ public class User {
 
     private Boolean enabled;
 
+    @JsonIgnore
     @ManyToMany
     @JoinTable(
             name = "user_role",
@@ -28,7 +29,9 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private List<Role> roles =new ArrayList<>();
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+//    @JsonIgnore
     private List<Board> boards = new ArrayList<>();
 
 }
